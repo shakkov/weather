553903eeab97b8e2bacf0811ef608ae2d93996fd
shakkov <52232760+shakkov@users.noreply.github.com>
2021-01-24 00:50:45 +0300

    upd

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const API_KEY='28ee77f51a584b67b69563dfa7f5dec4'
-const images = {
+const imagesWeather = {
     '-1': 'cold.jpg',
     '0': 'cold.jpg',
     '1': 'warm.jpg'
@@ -18,6 +18,10 @@ const months = {
     '10': 'Nov',
     '11': 'Dec'
 }
+const imagesTime = {
+    day : 'day.jpg',
+    night : 'night.jpg'
+}
 
 let date = new Date()
 
@@ -25,19 +29,29 @@ let date = new Date()
 
 document.getElementById('sendButton').addEventListener("click", async (event) => {
     event.preventDefault()
+    document.getElementById('hidden').style.display = 'none'
     const City = document.querySelector('#input').value
     const res = await fetch (`http://api.openweathermap.org/data/2.5/weather?q=${City}&APPID=${API_KEY}`)
     const data = await res.json()
+   
 
     if(data.cod !== '404') {
         const cels = Math.round(data.main.temp - 273.15)
 
-        document.querySelector('.wrapper').style.backgroundImage = `url(${images [Math.sign(cels)]})` 
+        document.querySelector('.wrapper').style.backgroundImage = `url(${imagesWeather [Math.sign(cels)]})` 
         document.getElementById('temperature').textContent = cels + '°c'
         document.getElementById('cityName').textContent = data.name
     } else {
-        alert('City not found :(')
+        document.getElementById('hidden').style.display = 'flex'
     }
 })
 
 document.querySelector('.time').textContent = date.getDate() +' ' +  (months[date.getMonth()])
+
+//date.setHours(11)
+
+if(date.getHours() <= 8 || date.getHours() >= 21  ){
+document.querySelector('.wrapper').style.backgroundImage = `url(${imagesTime ['night']})`
+} else{
+    document.querySelector('.wrapper').style.backgroundImage = `url(${imagesTime ['day']})`
+}
